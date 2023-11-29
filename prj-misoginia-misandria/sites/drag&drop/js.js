09cefc0df8e901fff573bec5727a551bc5d3654b
isaBELAxsilva <133895409+isaBELAxsilva@users.noreply.github.com>
2023-11-29 15:16:46 -0300

    Add files via upload

---
+++ prj-misoginia-misandria/sites/drag&drop/js.js
@@ -0,0 +1,57 @@
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
+
+
+
+function allowDrop2(ev) {
+    ev.preventDefault();
+}
+
+function drag2(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop2(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
+
+function allowDrop3(ev) {
+    ev.preventDefault();
+}
+
+function drag3(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop3(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
+
+function allowDrop4(ev) {
+    ev.preventDefault();
+}
+
+function drag4(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop4(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
